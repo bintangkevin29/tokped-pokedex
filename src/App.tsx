7c79d4acb1877d0fbbadfbe876cd152beed90d46
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-09 19:28:42 +0700

    added bottom nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import BottomNav from "./components/bottom-nav";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BottomNav />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/bottom-nav/bottom-nav.component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import "./bottom-nav.style.scss";
+
+const BottomNav: React.FC = () => {
+  return (
+    <div className="bottomNav">
+      <Container fluid>Tes</Container>
+    </div>
+  );
+};
+
+export default BottomNav;
