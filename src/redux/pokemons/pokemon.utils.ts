f0e336520395c80b3bc856e78c80ee4408e4147e
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-10 18:39:29 +0700

    add new pokemon details if not existing in the list

--- src/redux/pokemons/pokemon.utils.ts
+++ src/redux/pokemons/pokemon.utils.ts
@@ -1,12 +1,29 @@
 import { PokemonDetails, PokemonList } from "./pokemons";
+import { baseUrl } from "../../lib/constant";
 
 export const insertPokemonDetails = (
   oldState: PokemonList[],
   incomingDetails: PokemonDetails
 ): PokemonList[] => {
-  return oldState.map((pokemon) => {
-    if (incomingDetails.forms[0].name === pokemon.name) {
-      return { ...pokemon, detail: incomingDetails };
-    } else return pokemon;
-  });
+
+  const isExist = oldState.find((pokemon) => pokemon.name === incomingDetails.name);
+
+  if (isExist) {
+    return oldState.map((pokemon) => {
+      if (incomingDetails.name === pokemon.name) {
+        return { ...pokemon, detail: incomingDetails };
+      } else return pokemon;
+    });
+  } else {
+    return [
+      ...oldState,
+      {
+        name: incomingDetails.name,
+        detail: incomingDetails,
+        owned: false,
+        url: `${baseUrl}/pokemon/${incomingDetails.name}`,
+      },
+    ];
+  }
+  
 };
