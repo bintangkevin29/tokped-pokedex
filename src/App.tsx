0d67c21f25d23fa3be71e3bbb39531b757cee8e1
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-10 16:06:41 +0700

    moved data fetching to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Route } from "react-router-dom";
 
 import { selectModules } from "./redux/modules/modules.selector";
@@ -8,9 +8,48 @@ import BottomNav from "./components/bottom-nav";
 import TopBanner from "./components/top-banner";
 
 import "./styles/app.scss";
+import { selectPokedex } from "./redux/pokemons/pokemons.selector";
+import useFetch from "./lib/use-fetch";
+import { baseUrl } from "./lib/constant";
+import { appendPokedexList } from "./redux/pokemons/pokemons.actions";
 
 function App() {
   const modules = useSelector(selectModules);
+  const pokedexPokemons = useSelector(selectPokedex);
+
+  const dispatch = useDispatch();
+
+
+  const [offset, setOffset] = useState(0);
+
+  const pokedexList = useFetch(`${baseUrl}/pokemon?limit=20&offset=${offset}`);
+
+  const offsetAdd = () => {
+    setOffset(offset + 20);
+  };
+
+  useEffect(() => {
+    if (pokedexPokemons.length < 20 + offset && pokedexList.response) {
+      console.log("jalan");
+      dispatch(appendPokedexList(pokedexList.response.results));
+    }
+
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pokedexList.response]);
+
+  useEffect(() => {
+    const whenScrolledToBottom = () => {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        offsetAdd();
+      }
+    };
+
+    if (!pokedexList.fetching) {
+      window.addEventListener("scroll", whenScrolledToBottom, { passive: true });
+    }
+
+    return () => window.removeEventListener("scroll", whenScrolledToBottom);
+  });
 
   return (
     <div className="App">
--- src/pages/pokedex-page/pokedex-page.component.tsx
+++ src/pages/pokedex-page/pokedex-page.component.tsx
@@ -1,60 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { Container } from "react-bootstrap";
 
 import { selectPokedex } from "../../redux/pokemons/pokemons.selector";
 
-import { baseUrl } from "../../lib/constant";
-import useFetch from "../../lib/use-fetch";
-
 import PokemonList from "../../components/pokemon-list";
 
-import { appendPokedexList } from "../../redux/pokemons/pokemons.actions";
-
 import "./pokedex-page.style.scss";
-import CustomSpinner from "../../components/custom-spinner";
 
 const PokedexPage: React.FC = () => {
-  const dispatch = useDispatch();
-
   const pokedexPokemons = useSelector(selectPokedex);
-
-  const [offset, setOffset] = useState(0);
-
-  const pokedexList = useFetch(`${baseUrl}/pokemon?limit=20&offset=${offset}`);
-
-  const offsetAdd = () => {
-    setOffset(offset + 20);
-  };
-
-  useEffect(() => {
-    if (pokedexPokemons.length < 20 + offset && pokedexList.response) {
-      dispatch(appendPokedexList(pokedexList.response.results));
-    }
-
-    //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pokedexList.response]);
-
-  useEffect(() => {
-    const whenScrolledToBottom = () => {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        offsetAdd();
-        console.log("jalan");
-      }
-    };
-
-    if (!pokedexList.fetching) {
-      window.addEventListener("scroll", whenScrolledToBottom, { passive: true });
-    }
-
-    return () => window.removeEventListener("scroll", whenScrolledToBottom);
-  });
 
   return (
     <section className="pokedex">
       <Container>
         <PokemonList pokemons={pokedexPokemons} />
-        {pokedexList.fetching && <CustomSpinner />}
       </Container>
     </section>
   );
