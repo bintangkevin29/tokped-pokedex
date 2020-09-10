03600ffc4f4e9aa438efab1410ca8afd66719738
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-11 00:44:27 +0700

    added evolution fetch

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,9 +4,11 @@ import { baseUrl } from "./constant";
 export const fetchPokemonData = async (name: string) => {
   const fetchedDetails = await Axios(`${baseUrl}/pokemon/${name}`);
   const fetchedSpecies = await Axios(`${baseUrl}/pokemon-species/${name}`);
-
+  const fetchedEvolution = await Axios(`${baseUrl}/evolution-chain/${fetchedDetails.data.id}`);
+  
   const resDetails = fetchedDetails.data;
   const resSpecies = fetchedSpecies.data;
+  const resEvolution = fetchedEvolution.data;
 
-  return { details: resDetails, species: resSpecies };
+  return { details: resDetails, species: resSpecies, evolution: resEvolution };
 };
