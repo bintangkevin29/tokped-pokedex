f800fa45b425a7872b35c872e6129b5bd747c5d2
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-11 00:50:49 +0700

    fixed wrong evolutioni chain

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,8 @@ import { baseUrl } from "./constant";
 export const fetchPokemonData = async (name: string) => {
   const fetchedDetails = await Axios(`${baseUrl}/pokemon/${name}`);
   const fetchedSpecies = await Axios(`${baseUrl}/pokemon-species/${name}`);
-  const fetchedEvolution = await Axios(`${baseUrl}/evolution-chain/${fetchedDetails.data.id}`);
-  
+  const fetchedEvolution = await Axios(fetchedSpecies.data.evolution_chain.url);
+
   const resDetails = fetchedDetails.data;
   const resSpecies = fetchedSpecies.data;
   const resEvolution = fetchedEvolution.data;
