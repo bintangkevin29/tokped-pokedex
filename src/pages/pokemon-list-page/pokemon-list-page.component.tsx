5cf03307b4a5740087b3c64aea01883f5d657710
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-10 11:10:48 +0700

    added pokemon list component

---
+++ src/pages/pokemon-list-page/pokemon-list-page.component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./pokemon-list-page.style.scss";
+import { Container } from "react-bootstrap";
+
+const PokemonListPage: React.FC = () => {
+  return (
+    <section className="pokemonList">
+      <Container>Test</Container>
+    </section>
+  );
+};
+
+export default PokemonListPage;
