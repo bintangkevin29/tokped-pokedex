7ab06ea5d5803f31715395e3ece96a647e86053f
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-11 19:52:53 +0700

    add selectMyPokemonsByName selector

--- src/redux/my-pokemons/my-pokemons.selector.ts
+++ src/redux/my-pokemons/my-pokemons.selector.ts
@@ -4,3 +4,7 @@ import { createSelector } from "reselect";
 const myPokemonState = (state: RootState) => state.myPokemons;
 
 export const selectMyPokemons = createSelector([myPokemonState], (data) => data.myPokemons);
+
+export const selectMyPokemonsByName = (name: string) => {
+  return createSelector([myPokemonState], (data) => data.myPokemons.find((dt) => dt.name === name));
+};
