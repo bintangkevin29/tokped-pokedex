6515b35d6fde8cb65bc3b9295e0cb90c83b64d22
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-13 23:17:15 +0700

    added setup test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,21 +1,7 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
+import Enzyme from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-import { unmountComponentAtNode } from "react-dom";
-
-let container: any = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
+Enzyme.configure({
+  adapter: new EnzymeAdapter(),
+  disableLifecycleMethods: true,
 });
