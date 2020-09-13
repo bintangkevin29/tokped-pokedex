866836ac853716a17d4cfd8275218ed1a084c36e
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-13 23:43:31 +0700

    prevent redux logger from logging on test

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,7 +3,11 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./root.reducer";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV !== "test") {
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
