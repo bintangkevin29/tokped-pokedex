d3d4c9513a413a9f2d1d9d4fa56b10d913d611e0
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-14 16:57:54 +0700

    added warning if pokemon list empty

--- src/components/pokemon-list/pokemon-list.component.tsx
+++ src/components/pokemon-list/pokemon-list.component.tsx
@@ -6,6 +6,7 @@ import { MyPokemons } from "../../redux/my-pokemons/my-pokemons";
 import PokemonCard from "../pokemon-card";
 
 import "./pokemon-list.style.scss";
+import { Link } from "react-router-dom";
 
 interface Props {
   pokemons: MyPokemons[];
@@ -20,6 +21,12 @@ const PokemonLists: React.FC<Props> = ({ pokemons }) => {
             <PokemonCard pokemonData={pokemon} />
           </Col>
         ))}
+        {pokemons.length === 0 && (
+          <span className="pokemonList__warning">
+            You Haven't Caught Any Pokemon Yet. Navigate to <Link to="/">Pokèdex</Link> now to catch
+            some Pokèmons!
+          </span>
+        )}
       </Row>
     </div>
   );
