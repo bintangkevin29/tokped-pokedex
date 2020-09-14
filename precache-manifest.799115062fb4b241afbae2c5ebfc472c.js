bfac25f984967dafad0c77c1f81a98199e8e2546
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-14 16:49:42 +0700

    Updates

---
+++ precache-manifest.799115062fb4b241afbae2c5ebfc472c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ed43246a4baa11b5886e11e94ed99c",
+    "url": "/tokped-pokedex/index.html"
+  },
+  {
+    "revision": "ca6ab9ef11377bafa97a",
+    "url": "/tokped-pokedex/static/css/main.5f9587f6.chunk.css"
+  },
+  {
+    "revision": "5046398d1b6bf8e547de",
+    "url": "/tokped-pokedex/static/js/2.5aa72bfc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tokped-pokedex/static/js/2.5aa72bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6ab9ef11377bafa97a",
+    "url": "/tokped-pokedex/static/js/main.4d95cc9d.chunk.js"
+  },
+  {
+    "revision": "5c8ad9ec725c3defd55a",
+    "url": "/tokped-pokedex/static/js/runtime-main.9c7b9862.js"
+  },
+  {
+    "revision": "3cc77f7ae67a266ef3aec2cdc6a79d9f",
+    "url": "/tokped-pokedex/static/media/monda-v10-latin-regular.3cc77f7a.woff"
+  },
+  {
+    "revision": "ba09374268a0f382b04cbd95f4f2608f",
+    "url": "/tokped-pokedex/static/media/monda-v10-latin-regular.ba093742.woff2"
+  },
+  {
+    "revision": "d887f90ca46e7e69d766864fb4b7bd85",
+    "url": "/tokped-pokedex/static/media/monda-v10-latin-regular.d887f90c.eot"
+  },
+  {
+    "revision": "e2a2c2c9dec443fed2ab80a5b6503376",
+    "url": "/tokped-pokedex/static/media/monda-v10-latin-regular.e2a2c2c9.ttf"
+  },
+  {
+    "revision": "fef37e30d621b81da3edaf238d3d65f4",
+    "url": "/tokped-pokedex/static/media/monda-v10-latin-regular.fef37e30.svg"
+  },
+  {
+    "revision": "6b4ffa55fdd7e087c6990685887572b4",
+    "url": "/tokped-pokedex/static/media/pokeball.6b4ffa55.svg"
+  },
+  {
+    "revision": "254d58d191dc3d109fd1c9926f9f309f",
+    "url": "/tokped-pokedex/static/media/pokemon.254d58d1.svg"
+  }
+]);
