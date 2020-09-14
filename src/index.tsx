71d9542b76839e386bbfb2839f29efe5209bb988
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-14 16:48:26 +0700

    modified app basenaem

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate persistor={persist}>
-      <BrowserRouter>
+      <BrowserRouter basename="/tokped-pokedex">
         <App />
       </BrowserRouter>
     </PersistGate>
